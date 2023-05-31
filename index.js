bf86e703f77cec89449883172161ef0a7a5aeed8
Pedro Ventura <pedrovnt@gmail.com>
2023-05-30 19:52:08 -0700

    Objects

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-// Write your solution in this file!
+
+let employee = {
+    name: "John Doe",
+    streetAddress: "123 Main St"
+  };
+  
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+      ...employee,
+      [key]: value
+    };
+  }
+  
+  
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+  
+  
+  function deleteFromEmployeeByKey(employee, key) {
+    let newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+  }
+  
+  
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
+  
+  
+  console.log(employee);
+  
+  let updatedEmployee1 = updateEmployeeWithKeyAndValue(employee, "name", "Jane Smith");
+  console.log(updatedEmployee1);
+  
+  let updatedEmployee2 = destructivelyUpdateEmployeeWithKeyAndValue(employee, "streetAddress", "456 Oak Ave");
+  console.log(updatedEmployee2);
+  
+  let deletedEmployee1 = deleteFromEmployeeByKey(employee, "name");
+  console.log(deletedEmployee1);
+  
+  let deletedEmployee2 = destructivelyDeleteFromEmployeeByKey(employee, "streetAddress");
+  console.log(deletedEmployee2);
+
